1d476cbd989472bb5dfc3b54b9132d228e465b82
nathb2b@gmail.com <nathb2b@gmail.com>
2017-02-26 14:11:40 +0100

    ^

--- src/Rescope.js
+++ src/Rescope.js
@@ -4,21 +4,24 @@ let Rescope_factory = function Rescope_factory( scope ) {
         return function Rescope( ...argz ) {
             if ( this && this.constructor === Rescope )// using new
             {
-                let _Rescope      = Rescope_factory(argz[0]);
-                _Rescope.Store    = class ContextualStore extends Store {
+                let _Rescope     = Rescope_factory(argz[0]);
+                _Rescope.Store   = class ContextualStore extends Store {
                     constructor() {
                         super(argz[0], ...arguments)
                     }
                 };
-                _Rescope.fetch = fetch.bind(_Rescope, argz[0]);
-                _Rescope.context  = argz[0] || Store.staticContext;
+                _Rescope.fetch   = fetch.bind(_Rescope, argz[0]);
+                _Rescope.context = argz[0] || Store.staticContext;
                 return _Rescope;
             } else {
                 return Store.map(argz[0], argz[1], scope)
             }
         }
     },
-    fetch        = function fetch( context, cb ) {
+    setContext      = function setContext( context ) {
+
+    },
+    fetch           = function fetch( context, cb ) {
         context    = context || Store.staticContext;
         let stores = Object.keys(context);
         if ( !stores.length )
@@ -29,9 +32,9 @@ let Rescope_factory = function Rescope_factory( scope ) {
     },
     Rescope         = Rescope_factory(null);
 
-Rescope.Store    = Store;
-Rescope.fetch = ( cb ) => fetch(Store.staticContext, cb);
-Rescope.context  = Store.staticContext;
+Rescope.Store   = Store;
+Rescope.fetch   = ( cb ) => fetch(Store.staticContext, cb);
+Rescope.context = Store.staticContext;
 
 if ( typeof window != 'undefined' ) {
     window.Rescope = Rescope;

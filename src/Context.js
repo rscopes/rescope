02bc941e11d2daef0cf1178e93b083730c9cd2a3
nathb2b@gmail.com <nathb2b@gmail.com>
2017-08-14 20:47:44 +0200

    ^

--- src/Context.js
+++ src/Context.js
@@ -25,27 +25,146 @@
  */
 
 
-import Store from "./Store";
+var isString     = require('isstring'),
+    isArray      = require('isarray'),
+    EventEmitter = require('events'),
+    isNumber     = require('isnumber')
+    , isFunction = require('isfunction')
+    , Store      = require('./Store');
 
 let openContexts = {};
 
-class Context extends Store {
+class Context extends EventEmitter {
+    static openContexts = openContexts;
 
-    constructor() {
-        super(...arguments)
+    static getContext( key ) {
+        return openContexts[key] = openContexts[key] || new Context({});
+    };
+
+    constructor( ctx, state, datas ) {
+        super();
+        this.context   = {};
+        this.state     = {};
+        this.datas     = {};
+        this.__locks   = {all : 0};
+        this.__context = {};
+        this.map(ctx, state, datas);
     }
 
-    fork( context, cb ) {
+    map( ctx, state = {}, datas = {} ) {
+        Object.keys(ctx).forEach(
+            id => {
+                if ( this.__context[id] ) {
+                    console.warn("Rescope Context : ", id, " already exist in this context !");
+                    return;
+                }
+                this.__context[id] = ctx[id];
+                Object.defineProperty(
+                    this.context,
+                    id,
+                    (( ctx, id ) => (
+                        {
+                            get : () => Store.mountStore(id, ctx, state[id])
+                        }
+                    ))
+                    (ctx, id)
+                );
+                Object.defineProperty(
+                    this.state,
+                    id,
+                    (( ctx, id ) => (
+                        {
+                            get : () => (!isFunction(ctx[id]) ? ctx[id].state : undefined),
+                            set : ( v ) => (Store.mountStore(id, ctx, v))
+                        }
+                    ))
+                    (ctx, id)
+                );
+                Object.defineProperty(
+                    this.datas,
+                    id,
+                    (( ctx, id ) => (
+                        {
+                            get : () => (!isFunction(ctx[id]) ? ctx[id].datas : undefined)
+                        }
+                    ))
+                    (ctx, id)
+                );
+            }
+        )
+    }
+
+    bind( comp, querys ) {
 
     }
 
-    fetch( context, cb ) {
-        context    = context || Store.staticContext;
-        let stores = Object.keys(context);
-        if ( !stores.length )
-            return cb(null, context);
-        let mountAllStore = new Store(context);
-        mountAllStore.pull(stores, true);
-        mountAllStore.then(( state ) => cb(null, state, context));
+    serialize( flags_states = /.*/, flags_datas = /.*/ ) {
+        let ctx = this.__context, output = {state : {}, datas : {}};
+        Object.keys(ctx).forEach(
+            id => {
+                if ( isFunction(ctx[id]) )
+                    return;
+
+                let flags = ctx[id].constructor.flags;
+
+                flags = isArray(flags) ? flags : [flags || ""];
+
+                if ( flags.reduce(( r, flag ) => (r || flags_states.test(flag)), false) )
+                    output.state[id] = this.state[id];
+
+                if ( flags.reduce(( r, flag ) => (r || flags_datas.test(flag)), false) )
+                    output.datas[id] = this.datas[id];
+            }
+        )
+        return output;
+    }
+
+    restore( {state, datas}, quiet ) {
+        let ctx = this.__context;
+        Object.keys(datas).forEach(
+            id => {
+                quiet ?
+                ctx.datas = datas[id]
+                    :
+                ctx.push(datas[id]);
+            }
+        );
+        Object.keys(state).forEach(
+            id => {
+                quiet ?
+                ctx.state = state[id]
+                    :
+                ctx.setState(state[id]);
+            }
+        );
+    }
+
+    lock( reason ) {
+        this.__locks.all++;
+        if ( reason ) {
+            this.__locks[reason] = this.__locks[reason] || 0;
+            this.__locks[reason]++;
+        }
+    }
+
+    dispose( reason ) {
+        this.__locks.all--;
+        if ( reason ) {
+            this.__locks[reason] = this.__locks[reason] || 0;
+            this.__locks[reason]--;
+        }
+        if ( !this.__locks.all )
+            this.destroy();
+    }
+
+    destroy() {
+        let ctx = this.__context;
+        Object.keys(ctx).forEach(
+            id => {
+                if ( isFunction(ctx[id]) || this.context !== ctx[id].context )
+                    return;
+                ctx[id].destroy()
+            }
+        )
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,67 @@ var production = process.argv.indexOf("--production") > -1
 module.exports = [
     {
         entry   : {
+            "Rescope" : "./src/Rescope.js",
+        },
+        devtool : 'source-map',
+        // description de nos sorties
+        output  : {
+            path       : __dirname,
+            filename   : "dist/[name].js",
+            publicPath : "/",
+        },
+
+        resolve : {
+            extensions : [
+                "",
+                ".js",
+                ".json",
+            ],
+        },
+
+        module  : {
+            loaders : [
+                {
+                    test    : /\.js$/,
+                    exclude : /node_modules/,
+                    loader  : 'babel-loader',
+                    query   : {
+
+                        presets : [
+                            'babel-preset-react',
+                            'babel-preset-es2015',
+                            'babel-preset-stage-0'
+                        ].map(require.resolve),
+                        plugins : [
+                            "babel-plugin-add-module-exports",
+                        ].map(require.resolve)
+                    }
+                },
+                {
+                    test    : /\.json$/,
+                    loaders : [
+                        "json",
+                    ],
+                },
+                {
+                    test    : /\.(html|txt)$/,
+                    loaders : [
+                        "file-loader?name=[path][name].[ext]&context=./src",
+                    ],
+                },
+            ],
+        },
+        plugins : (
+            [
+                new webpack.DefinePlugin({
+                    __PROD__ : production
+                }),
+            ]
+        ),
+
+    },
+    {
+        entry   : {
             "example/vanilla/NewsListComp" : ["./src/example/vanilla/NewsListComp.js", './src/example/vanilla/index.html'],
             "example/react/App" : ["./src/example/react/App.js", './src/example/react/index.html'],
         },

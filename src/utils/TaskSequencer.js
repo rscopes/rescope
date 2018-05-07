7dff45da0a538b7c61b704b6702556723b8ea6ea
nathb <nathb2b@gmail.com>
2018-05-07 14:45:49 +0200

    ^ allow es6 child constructors to set state, data, ^ make es6 constructor defined fn ( eg: event = ()=>{/*...*/} ) on initial apply

--- src/utils/TaskSequencer.js
+++ src/utils/TaskSequencer.js
@@ -131,7 +131,7 @@ export default {
         curWeight = Math.min(curWeight, weight);
         taskCount++;
         
-        console.log("Push Task : ", fn, " on ", obj.name, weight);
+        //console.log("Push Task : ", fn, " on ", obj.name, weight);
         stack.push([ obj, fn, argz ]);
         setTimeout(runNow, 0);
         return stack.length;
